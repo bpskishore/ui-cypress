de8592cb962b8ee2aed78cd8a5a02e7302e1252d
bpskishore <bpskishore@gmail.com>
2020-01-12 13:22:47 -0500

    test commit

--- cypress/integration/Google/googleEx.js
+++ cypress/integration/Google/googleEx.js
@@ -8,6 +8,7 @@ context('Actions', () => {
     it('validate google search', () => {
         cy.get('.gLFyf').type('I love love love LOVE Test & Automation!').type('{enter}');
         cy.title().should('include', 'I love love love LOVE Test & Automation!');
+        cy.title().should('not.include', 'asdf');
 
     });
 
